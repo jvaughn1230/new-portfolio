4225de784cb07ef6a0e7f93d6aa7ff200d2f04d6
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-07-26 15:23:20 -0700

    adding about text

--- src/Containers/About.jsx
+++ src/Containers/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div className='flex justify-center'>
       <h2 className='text-4xl'>About Me</h2>
-      <div>Testing</div>
+      <div className='text-xl'>I'm a full stack developed based in Las Vegas NV. I build beautiful responsive websites with scalability. Depending on the project, I use a mix of React, HTML, CSS, JavaScript, Bootstrap, Node.js and Express.js. I thoroughly enjoy solving complex problems and learning new programming languages. When I'm not programming, I can usually be found out hiking! </div>
     </div>
   )
 }
